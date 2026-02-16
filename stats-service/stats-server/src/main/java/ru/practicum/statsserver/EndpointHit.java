9feb20b450cc61f9016769b7728a889b618bc52c
verp205 <s4naumenko@yandex.ru>
2026-02-16 19:17:25 +0300

    кодстайл и исправление докер

--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/EndpointHit.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/EndpointHit.java
@@ -1,8 +1,6 @@
 package ru.practicum.statsserver;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- stats-service/stats-server/src/main/java/ru/practicum/statsserver/EndpointHitRepository.java
+++ stats-service/stats-server/src/main/java/ru/practicum/statsserver/EndpointHitRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.statsserver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import ru.practicum.ViewStatsDto;
 
 import java.time.LocalDateTime;
 import java.util.List;
